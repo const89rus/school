65d1ba83719cd3da64df7b92b6ef6f7d51ffe70e
Vadim <vadim_nojabrs@mail.ru>
2020-11-02 20:00:52 +0300

    init

--- src/view/pages/MainPage/MainPage.tsx
+++ src/view/pages/MainPage/MainPage.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
+import { Header, Team } from 'view/components/mainPage';
+
 export const MainPage: React.FC<RouteComponentProps> = () => {
-  return <div>1</div>;
+  return (
+    <div>
+      <Header />
+      <Team />
+    </div>
+  );
 };
