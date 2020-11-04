4f4afaeb3779b3b328e8b1b0fdf5926918196f53
Vadim <vadim_nojabrs@mail.ru>
2020-11-04 16:44:14 +0300

    add layout header

--- src/view/components/layout/Layout.tsx
+++ src/view/components/layout/Layout.tsx
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 
-import { Button } from 'view/common';
+import { ReactComponent as Logo } from 'assets/svg/logo.svg';
+import { ReactComponent as Vk } from 'assets/svg/vk.svg';
+import { ReactComponent as Insta } from 'assets/svg/insta.svg';
 
 import * as S from './styled';
 
@@ -8,7 +10,18 @@ export const Layout: React.FC = ({ children }) => {
   return (
     <Fragment>
       <S.Layout>
-        <Button>12</Button>
+        <S.LogoContainer>
+          <Logo />
+        </S.LogoContainer>
+
+        <S.Button>Личный кабинет</S.Button>
+
+        <S.SocialContainer>
+          <Vk />
+        </S.SocialContainer>
+        <S.SocialContainer>
+          <Insta />
+        </S.SocialContainer>
       </S.Layout>
       {children}
     </Fragment>
--- src/view/components/layout/styled.ts
+++ src/view/components/layout/styled.ts
@@ -1,6 +1,36 @@
 import styled from 'styled-components';
 
+import { Button as ButtonUi } from 'view/common';
+
 export const Layout = styled.div`
   background-color: #1794a5;
   height: 10.313rem;
+  display: flex;
+  padding-left: 23.5rem;
+  padding-right: 23.438rem;
+`;
+
+export const Button = styled(ButtonUi)`
+  color: #ffffff;
+  height: 4.063rem;
+  width: 23.125rem;
+  font-size: 2rem;
+  border: none;
+  margin-left: 20.75rem;
+  margin-top: 2.625rem;
+`;
+
+export const SocialContainer = styled.div`
+  margin-left: 1.875rem;
+  background-color: #075f6b;
+  height: 4.063rem;
+  width: 4.375rem;
+  margin-top: 2.625rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LogoContainer = styled.div`
+  margin-top: 1.625rem;
 `;
