25bd3b25ca7f7a0f22b618fe019fa2f366b590c6
Booseel <40848968+Booseel@users.noreply.github.com>
2020-11-14 02:48:54 +0500

    change display on tabs

--- src/view/components/mainPage/Tabs/components/styled.ts
+++ src/view/components/mainPage/Tabs/components/styled.ts
@@ -4,6 +4,8 @@ export const Wrapper = styled.div`
   padding: ${(props) => props.theme.root.contentPadding};
   padding-top: 5.625rem;
   padding-bottom: 20rem;
-  display: flex;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   justify-content: space-around;
 `;
